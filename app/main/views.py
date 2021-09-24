63053e675480e88378e44cb0e176d5eb6b4d1876
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-24 21:10:20 +0300

    Add root view function

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from . import main
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Home - Hello Moses...'
+    return render_template('index.html', message = message)
