d4aecf8854626e063dd31a8a73f95bb23517a6ad
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-25 08:51:40 +0300

    Add decorator login manager

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin,db.Model):
@@ -32,4 +38,6 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
     
