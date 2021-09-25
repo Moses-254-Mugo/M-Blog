28f0de7b2e7307127eab5c0fbddb6f042a15087a
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-25 08:45:42 +0300

    Import the login manager class

--- manage.py
+++ manage.py
@@ -1,11 +1,20 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 
+
 app = create_app('development')
-manager = Manager(app)
+
+manager = Manager(app,db)
+manage = Manager(app)
+
 manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+
+
 @manager.shell
 def make_shell_context():
     return dict(app=app, db = db, User = User)
